633e049f8f214a1fc5e0b540507720566c9a7e31
salimoglu <salimoglu61@gmail.com>
2026-04-04 09:48:48 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -29,6 +29,7 @@
     if (typeof UrunModule !== "undefined") UrunModule.init();
     if (typeof BirikimModule !== "undefined") BirikimModule.init();
   if (typeof AltinModule !== "undefined") AltinModule.init();
+  if (typeof VefaModule !== "undefined") VefaModule.init();
   }
 
   // Logo tiklama: Firebase'den taze cek, modulleri yenile, sifre sorma
@@ -146,6 +147,7 @@
     if (btn.dataset.tab === "alacaklar" && typeof AlacaklarModule !== "undefined") AlacaklarModule.init();
     if (btn.dataset.tab === "urun" && typeof UrunModule !== "undefined") UrunModule.init();
     if (btn.dataset.tab === "altin" && typeof AltinModule !== "undefined") AltinModule.init();
+    if (btn.dataset.tab === "vefa" && typeof VefaModule !== "undefined") VefaModule.init();
   }));
 
   // Animasyonlar
